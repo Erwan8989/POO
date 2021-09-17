c44bbfaad1991b98a6524800a168da78dcb57daa
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2021-09-17 10:09:31 +0200

    Update classe.php

--- classe.php
+++ classe.php
@@ -1,15 +1,19 @@
 <?php
 
-class Animal { 
-    public int $nmbdepattes = 0;
+
+
+class Animal {     
 }
 
-$Animal1  = new Animal();
+$Animal1  = new Animal();  // Instanciation de la classe dans une variable, pour la récupérer plus tard
 $Animal2 = new Animal();
 
-var_dump($Animal1);
-var_dump($Animal2);
+// var_dump($Animal1);
+// var_dump($Animal2);
 
-echo get_class($Animal1);
+var_dump(get_class($Animal1));
+
+
+// echo get_class($Animal1); // N'affiche que des chaines de caractères
 
 ?>
