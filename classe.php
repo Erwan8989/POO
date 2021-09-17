5b837e0f2cb067b8a05f573d4a26417af836eb02
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2021-09-17 12:37:58 +0200

    New private file

--- classe.php
+++ classe.php
@@ -3,28 +3,13 @@
 
 
 class Animal {     
-<<<<<<< Updated upstream
-    function __construct() {
-    
-        echo "Un animal vient de naitre ! <br>" . PHP_EOL;
-        $this-> age = 0; // Permet de modifier l'instance sur lequel on travail
-
-=======
     function __construct(string $nom = '', int $age = 0) { 
         $this-> age = $age; // Permet de modifier l'instance sur lequel on travail
         $this->nommer($nom); 
->>>>>>> Stashed changes
     }
 
     function vieillir() {
         $this->age = $this->age +1;
-<<<<<<< Updated upstream
-    }
-}
-
-$Animal1  = new Animal(); // Instanciation de la classe dans une variable, pour la récupérer plus tard
-$Animal2 = new Animal();
-=======
         return $this;
     }
 
@@ -37,29 +22,20 @@ $Animal2 = new Animal();
 }
 
 // $Animal1  = new Animal(20); // Instanciation de la classe dans une variable, pour la récupérer plus tard
-$Animal2 = new Animal('Milou', 4);
+$Animal1 = new Animal('Milou', 4);
 // $Animal1->nommer('Jean');
->>>>>>> Stashed changes
 
 // var_dump($Animal1);
 // var_dump($Animal2);
 
-<<<<<<< Updated upstream
-$Animal1->age = 2;
-$Animal2->vieillir();
-
-var_dump($Animal1->age);
-var_dump($Animal2);
-=======
 // $Animal1->age;
 // $Animal2 ->vieillir()->vieillir()->vieillir();
 
-echo "L'animal 1 est agé de ".$animal1->age." ans, et s'appelle ".$animal1->nom."<br> <br>"; PHP_EOL;
+echo "L'animal 1 est agé de ".$Animal1->age." ans, et s'appelle ".$Animal1->nom."<br> <br>"; PHP_EOL;
 // echo "L'age de l'animal 2 est de : $Animal2->age ans.". PHP_EOL;
 
 
 var_dump($Animal1);
->>>>>>> Stashed changes
 
 // echo get_class($Animal1); // N'affiche que des chaines de caractères
 
