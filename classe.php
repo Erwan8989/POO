b38d6fec4518ef4f9521f35f4e93e9c598d42653
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2021-09-17 10:20:22 +0200

    Update classe.php

--- classe.php
+++ classe.php
@@ -3,16 +3,21 @@
 
 
 class Animal {     
+    function __construct()
+    {
+        echo "Un animal vient d'être crée <br>" . PHP_EOL;
+    }
 }
 
-$Animal1  = new Animal();  // Instanciation de la classe dans une variable, pour la récupérer plus tard
-$Animal2 = new Animal();
+$Animal1  = new Animal(); // Instanciation de la classe dans une variable, pour la récupérer plus tard
+
+$Animal2  = new Animal();
+
 
 // var_dump($Animal1);
 // var_dump($Animal2);
 
 var_dump(get_class($Animal1));
-
 
 // echo get_class($Animal1); // N'affiche que des chaines de caractères
 
