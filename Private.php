c91d0ab9b9849bbd82d9af6fe19ddfe4da535e3f
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2021-09-17 12:59:44 +0200

    Update Private.php

--- Private.php
+++ Private.php
@@ -1,8 +1,30 @@
 <?php
 
 
+class Animal {
+    private $age;
 
+    function __construct()
+    {
+        $this->age = 0;
+    }
 
+    function viellir() {
+        $this->age = $this->age + 1;
+    }
+
+    public function getAge() {
+        $this->age;
+        return $this;
+    }
+
+}
+
+$animal = new Animal();
+$animal->viellir();
+
+echo ($animal->getAge());
+// var_dump($animal->getAge());
 
 
 ?>
