7d7a7cf2cb36b9bf01ce65e835c158d7c5d98741
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2021-09-17 11:44:55 +0200

    Update classe.php

--- classe.php
+++ classe.php
@@ -9,14 +9,23 @@ class Animal {
         $this-> age = 0; // Permet de modifier l'instance sur lequel on travail
 
     }
+
+    function vieillir() {
+        $this->age = $this->age +1;
+    }
 }
 
 $Animal1  = new Animal(); // Instanciation de la classe dans une variable, pour la récupérer plus tard
+$Animal2 = new Animal();
 
 // var_dump($Animal1);
 // var_dump($Animal2);
 
+$Animal1->age = 2;
+$Animal2->vieillir();
+
 var_dump($Animal1->age);
+var_dump($Animal2);
 
 // echo get_class($Animal1); // N'affiche que des chaines de caractères
 
