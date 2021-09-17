cb32b0784dbc097b905fe66a98fc9740dbc8b4a6
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2021-09-17 15:06:44 +0200

    Update Private.php

--- Private.php
+++ Private.php
@@ -6,6 +6,9 @@ class Animal {
     function __construct(int $age = 0, string $nom = '')
     {
         $this->age = 0;
+        $this->nom ='';
+        echo 'instanciation de la classe reptile' . PHP_EOL;
+       
     }
 
     function viellir() {
@@ -26,8 +29,17 @@ class Animal {
 class Reptile extends Animal {
     public function __construct(){
         parent::__construct();
+        $this-> nombreEcaille = 1000;
+        echo 'instanciation de la classe reptile' . PHP_EOL;
+    }
+
+    public function nommer($nom) {
+        echo 'On nomme depuis la classe Reptile' . PHP_EOL;
+        parent::nommer($nom);
     }
 }
+
+
 
 $reptile = new Reptile();
 
@@ -35,10 +47,10 @@ var_dump('class de reptile');
 var_dump(get_class($reptile));
 var_dump($reptile->age);
 
-
 var_dump('On renomme reptile');
 $reptile->nommer('kaa');
 var_dump($reptile->nom);
+
 
 // $animal = new Animal();
 // $animal->viellir();
