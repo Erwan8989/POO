2275493d023a2ec4ae701cc66f797cd163d18712
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2021-09-18 09:07:52 +0200

    Update Exercice.php

--- Exercice.php
+++ Exercice.php
@@ -1,21 +1,65 @@
 <?php
-// $solde = 0;
 
-// $solde = $_GET['nom'];
+class Animal {
+    private $nom;
+    private $age;
 
+    public function __construct(string $nom = "", int $age = 0)
+    {
+        echo "Un nouvelle animal a été crée <br>";   
+        $this->setNom($nom);
+        $this->setAge($age);
+    }
 
-// if($solde > 0){
-//     echo "Votre solde est positif !";
-// } else if ($solde < 0){
-//     echo "Votre solde est négatif !";
-// } else{
-//     echo "Votre solde est nul";
-// }
+    public function getAge(){
+        return $this->age;
+    }
 
-$tab = array('Pierre', 'Paul', 'Jack');
+    public function getNom(){
+        return $this->nom;
+    }
 
-$tab2 = array('prenom' => $tab[1], 'metier' => 'dev');
+    public function setNom(string $nom){
+        $this->nom = $nom;
+    }
 
-echo $tab2['prenom']. " a le métier ". $tab2['metier'];
+    public function setAge(int $age){
+        $this->age = $age;
+    }
 
+    public function vieillir(){
+        $this->age = $this->age + 1;
+        return $this;
+    }
+}
 
+class Reptile extends Animal{
+    private $espece;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->espece = "Reptile";
+        echo "Un nouveau reptile a été créé <br>";
+    }
+
+    public function getEspece(){
+        return $this->espece;
+    }
+}
+
+$animal1 = new Reptile;
+
+echo "L'animal est agé de ".$animal1->getAge(). " ans et s'appelle ".$animal1->getNom()."<br>";
+
+$animal1->setAge(5);
+$animal1->setNom('Babar');
+echo "L'animal est agé de ".$animal1->getAge(). " ans et s'appelle ".$animal1->getNom()."<br>";
+
+$animal1->vieillir();
+echo "L'animal est agé de ".$animal1->getAge(). " ans et s'appelle ".$animal1->getNom()."<br>";
+
+$animal1->vieillir()->vieillir();
+echo "L'animal est agé de ".$animal1->getAge(). " ans et s'appelle ".$animal1->getNom()."<br>";
+    
+echo $animal1->getEspece();
