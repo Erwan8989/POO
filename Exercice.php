57498cecffcb44c836e383f381530bb49fb41290
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2021-09-20 14:23:07 +0200

    Update Exercice.php

--- Exercice.php
+++ Exercice.php
@@ -6,9 +6,9 @@ class Animal {
     
     protected $espece;
 
-    public function __construct(string $nom = "", int $age = 0)
+    public function __construct(string $nom = "Michel", int $age = 0)
     {
-        echo "Un nouvelle animal a été crée <br>";   
+        echo "Un nouvelle animal a été crée <br>". PHP_EOL;      
         $this->setNom($nom);
         $this->setAge($age);
     }
@@ -22,7 +22,7 @@ class Animal {
     }
 
     public function setNom(string $nom){
-        echo "Nommé depuis la classe Animal <br>";
+        echo "Nommé depuis la classe Animal <br>" . PHP_EOL; 
         $this->nom = $nom;
 
     }
@@ -38,12 +38,12 @@ class Animal {
 }
 
 class Reptile extends Animal{
-
     public function __construct()
+
     {
         parent::__construct();
         $this->espece = "Reptile";
-        echo "Un nouveau reptile a été créé <br>";
+        echo "Un nouveau reptile a été créé <br>" . PHP_EOL; 
     }
 
     public function getEspece(){
@@ -51,24 +51,29 @@ class Reptile extends Animal{
     }
 
     public function setNom(string $nom){
-        echo "Nommé depuis la classe Reptile <br>";
+        echo "Nommé depuis la classe Reptile <br>" . PHP_EOL; 
         $this->nom = $nom;
     }
+
+    public $nom;
 }
 
 $animal1 = new Reptile;
+// $animal2 = new Animal;
 
-echo "L'animal est agé de ".$animal1->getAge(). " ans et s'appelle ".$animal1->getNom()."<br>";
+echo "<br> L'animal est agé de ".$animal1->getAge(). " ans et s'appelle ".$animal1->getNom()."<br>" . PHP_EOL; 
+// $animal2->setNom('yang');
 
 $animal1->setAge(5);
-$animal1->setNom('Babar');
-echo "L'animal est agé de ".$animal1->getAge(). " ans et s'appelle ".$animal1->getNom()."<br>";
+// $animal1->setNom('Babar');
+
+echo "L'animal est agé de ".$animal1->getAge(). " ans et s'appelle ".$animal2->getNom()."<br>" . PHP_EOL; 
 
 $animal1->vieillir();
-echo "L'animal est agé de ".$animal1->getAge(). " ans et s'appelle ".$animal1->getNom()."<br>";
+echo "L'animal est agé de ".$animal1->getAge(). " ans et s'appelle ".$animal2->getNom()."<br>" . PHP_EOL; 
 
 $animal1->vieillir()->vieillir();
-echo "L'animal est agé de ".$animal1->getAge(). " ans et s'appelle ".$animal1->getNom()."<br>";
+echo "L'animal est agé de ".$animal1->getAge(). " ans et s'appelle ".$animal2->getNom()."<br>" . PHP_EOL; 
     
 echo $animal1->getEspece();
 
