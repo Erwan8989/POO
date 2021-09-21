ec06a36006ad621eb9119847299960019dd79b6d
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2021-09-21 14:47:29 +0200

    Cours, exs

--- Exercice_deuxieme_cours.php
+++ Exercice_deuxieme_cours.php
@@ -1,2 +1,68 @@
 <?php
+  
+  /*
+class Admin {
+    protected static $ban;
+    public const ABONNEMENT = 5;
+        
+    public function __construct($name, $prenom, $region){
+        $this->user_name = strtoupper($name);
+        $this->user_pass = $prenom;
+        $this->user_region = $region;
+    }
 
+    public static function setBan(...$b){
+        foreach($b as $banned){
+            self::$ban[] .= $banned;
+        }
+    }
+
+    public static function getBan(){
+    echo 'Utilisateurs bannis : ' ;
+    foreach(self::$ban as $valeur){
+        echo $valeur .', ';
+    }
+    }
+
+    public function setPrixAbo(){
+        return $this->prix_abo = self::ABONNEMENT;
+    }
+
+}
+
+$test = new Admin('Durant', 'Toto', 'IDF');
+
+$test->setBan('test');
+
+
+$test->getBan();
+*/
+/*
+interface Foo 
+{
+    function a();
+}
+interface Bar 
+{
+    function b();
+}
+
+class FooBar implements Foo, Bar 
+{
+    function a() 
+    {
+        print "aaaaaaaa<br/>";
+    }
+    function b() 
+    {
+       print "bbbbbbb<br/>";
+    }
+}
+
+$aa = new FooBar;
+echo "<b>Appel fonction a</b><br/>";
+$aa->a();
+echo "<b>Appel fonction b</b><br/>";
+$aa->b();
+
+*/
---
+++ add_moy.php
@@ -0,0 +1,42 @@
+
+<?php
+class Addition {
+    public function __construct(){
+        
+    }
+
+    public function calcul($a, $b){
+        $this->result = $a + $b;
+        return $this;
+    } 
+}
+
+class Moyenne extends Addition {
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public int $moy;
+
+    public function moyenne($a, $b){
+        parent::calcul($a, $b);
+        $this->moy = $this->result /= 2;
+        
+    }
+
+    public function getMoyenne() {
+        echo "<br>La moyenne vaut ".$this->moy;
+    }
+}
+
+$obj1 = new Addition();
+$obj1->calcul(10, 20);
+
+echo $obj1->result;
+
+
+$obj2 = new Moyenne();
+$obj2->moyenne(10, 20);
+
+$obj2->getMoyenne();
