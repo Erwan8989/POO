05f1c6a9d8c66777fda3ec7b6dca2c8d9b11a36c
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2021-09-21 16:04:36 +0200

    Update Addition-exo-10.php

--- Exo/Addition-exo-10.php
+++ Exo/Addition-exo-10.php
@@ -1,39 +1,6 @@
 <?php
 
 class Addition {
-<<<<<<< Updated upstream
-    private $_valeur1 , $_valeur2;
-    private $valeur, $i, $moyenne;
-
-    public function __construct($valeur1, $valeur2){
-        $this->_valeur1 = $valeur1;
-        $this->_valeur2 = $valeur2;
-    }
-
-    public function addition(){
-        return $this->_valeur1 + $this->_valeur2;
-    }
-
-    public function Moyenne(...$i){
-        foreach ($i as $Valeur) {
-            $this-> somme += $Valeur;
-            $i++;
-        }
-    }
-=======
-    // private $_valeur1 , $_valeur2;
-   
-
-    // public function __construct($valeur1, $valeur2){
-    //     $this->_valeur1 = $valeur1;
-    //     $this->_valeur2 = $valeur2;
-    // }
-   
-
-    // public function addition(){
-    //     return $this->_valeur1 + $this->_valeur2;
-    // } }
-
     public function __construct() {
 
     }
@@ -43,8 +10,7 @@ class Addition {
     }
 }
 
-
-class Moyenne extends Addition {
+class Average extends Addition {
     public function __construct() {
         parent::__construct();
     }
@@ -54,7 +20,24 @@ class Moyenne extends Addition {
         return $sum / 2;
     }
 }
->>>>>>> Stashed changes
+    // private $_valeur1 , $_valeur2;
+    // private $valeur, $i, $moyenne;
+
+    // public function __construct($valeur1, $valeur2){
+    //     $this->_valeur1 = $valeur1;
+    //     $this->_valeur2 = $valeur2;
+    // }
+
+    // public function addition(){
+    //     return $this->_valeur1 + $this->_valeur2;
+    // }
+
+    // public function Moyenne(...$i){
+    //     foreach ($i as $Valeur) {
+    //         $this-> somme += $Valeur;
+    //         $i++;
+    //     }
+    // }
 
     // public function soustraction(){
     //     return $this->_valeur1 - $this->_valeur2;
@@ -67,15 +50,15 @@ class Moyenne extends Addition {
     // public function division () {
     //     return $this->_valeur1 / $this->_valeur2;
     // }
-<<<<<<< Updated upstream
+
 }
 
-$calcule = new Addition(3,4);
-echo "<p>3 + 4 = ".$calcule->addition(). "</p>";
+// $calcule = new Addition(3,4);
+// echo "<p>3 + 4 = ".$calcule->addition(). "</p>";
 
-$calcule = new Addition(10, 15);
-echo "<p>10 + 15 = ".$calcule->Moyenne(). "</p>";
-=======
+// $calcule = new Addition(10, 15);
+// echo "<p>10 + 15 = ".$calcule->Moyenne(). "</p>";
+
 
     $Object_Addition = new Addition();
     var_dump($Object_Addition->sum(4,3));
@@ -90,7 +73,6 @@ echo "<p>10 + 15 = ".$calcule->Moyenne(). "</p>";
 // $calcule2 = new Moyenne(); 
 // echo "<p>15 + 20 = ".$calcule2->moyenne(15,20). "</p>";
 
->>>>>>> Stashed changes
 
 // $calcule = new Addition (15,12);
 // echo "<p>15 - 12 = ".$calcule->soustraction(). "</p>";
